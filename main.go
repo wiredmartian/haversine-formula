5fc7373c6b073f88cde780cc2a4e48ec0a512333
wiredmartian <meggatym@yahoo.com>
2021-04-08 22:27:17 +0200

    test function

--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
 /* HAVERSINE FORMULA
-	a = sin²(φB - φA/2) + cos φA * cos φB * sin²(λB - λA/2)
-	c = 2 * atan2( √a, √(1−a) )
-	d = R ⋅ c
+a = sin²(φB - φA/2) + cos φA * cos φB * sin²(λB - λA/2)
+c = 2 * atan2( √a, √(1−a) )
+d = R ⋅ c
 */
 import (
 	"fmt"
 	"math"
 )
 
-func main()  {
-	fmt.Println("Hello from go")
+func main() {
+	fmt.Println("---------Hello from go---------")
 
 	// ==== Test formula
-
-	// point 1 GPS coordinates
-	point1 := Coordinates {
-		latitude: 51.510357,
-		longitude: -0.116773,
-	}
-
-	// point 2 GPS coordinates
-	point2 := Coordinates {
-		latitude: 38.889931,
-		longitude: -77.009003,
-	}
-
-	// distance in km
-	distance := haversine(point1, point2)
-
-	fmt.Printf("Distance is %.3fkm", distance)
+	runTest()
 }
 
 // Coordinates coordinates struct
 type Coordinates struct {
-	latitude float64
+	latitude  float64
 	longitude float64
 }
 
@@ -49,15 +33,48 @@ func haversine(point1 Coordinates, point2 Coordinates) float64 {
 	phi1 := degToRadians(point1.latitude)
 	phi2 := degToRadians(point2.latitude)
 
-	a := math.Sin(dlat / 2) * math.Sin(dlat / 2) +
-		 math.Cos(phi1) * math.Cos(phi2) *
-		 math.Sin(dlon / 2) * math.Sin(dlon / 2)
+	a := math.Sin(dlat/2)*math.Sin(dlat/2) +
+		math.Cos(phi1)*math.Cos(phi2)*
+			math.Sin(dlon/2)*math.Sin(dlon/2)
 
-	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1 - a))
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	return c * R
 }
 
 // convert degrees to radians
 func degToRadians(degrees float64) float64 {
 	return degrees * math.Pi / 180
+}
+
+// test formula
+func runTest() {
+	// point 1 GPS coordinates
+	point1 := Coordinates{
+		latitude:  51.510357,
+		longitude: -0.116773,
+	}
+
+	// point 2 GPS coordinates
+	point2 := Coordinates{
+		latitude:  38.889931,
+		longitude: -77.009003,
+	}
+
+	// point 3 GPS coordinates (Morningside)
+	point3 := Coordinates{
+		latitude:  31.0118578,
+		longitude: -29.8173733,
+	}
+
+	// point 4 GPS coordinates (DUT Ritson Campus)
+	point4 := Coordinates{
+		latitude:  31.0056468,
+		longitude: -29.8518299,
+	}
+	// distance in km
+	distance1 := haversine(point1, point2)
+	distance2 := haversine(point3, point4)
+
+	fmt.Printf("\nDistance is %.3fkm", distance1)
+	fmt.Printf("\n\nStraight line distance between Morningside and DUT: %.2fkm\n", distance2)
 }
